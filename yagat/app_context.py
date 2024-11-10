df225d320b19c7ae20534f04bc4c2ef8a66eb340
Damien Jeandemange <damien.jeandemange@gmail.com>
2024-11-10 11:29:53 +0100

    Fix
    This reverts commit 9f17835ca12c20e39df30f519d9cb795bc5ce33e.
    
    Signed-off-by: Damien Jeandemange <damien.jeandemange@gmail.com>

--- yagat/app_context.py
+++ yagat/app_context.py
@@ -91,7 +91,7 @@ class AppContext:
 
     @selection.setter
     def selection(self, value: tuple[Optional[str], Optional[str], Optional[ns.Connection]]) -> None:
-        logging.info(f'selection setter called {value[0]},{value[1]}')
+        logging.info(f'selection setter called {value}')
         self._selection = value
         self.notify_selection_changed()
 
