cf635ddee18299f14d0dcfcf4c9d31c34550a8b1
jeandemanged <damien.jeandemange@gmail.com>
2024-11-20 23:43:59 +0100

    Long-running tasks in background thread (#16)
    Signed-off-by: Damien Jeandemange <damien.jeandemange@gmail.com>

--- yagat/menus/impl/run.py
+++ yagat/menus/impl/run.py
@@ -20,14 +20,24 @@ class RunMenu(tk.Menu):
         self.parent = parent
         self.context = context
         parent.add_cascade(label="Run", menu=self)
-        self.add_command(label='Load Flow', command=self.run_load_flow)
+        self.add_command(label='AC Load Flow', command=lambda: self.run_load_flow(ac=True))
+        self.add_command(label='DC Load Flow', command=lambda: self.run_load_flow(ac=False))
 
-    def run_load_flow(self):
+    def run_load_flow(self, ac: bool):
         reporter = pr.Reporter()
         self.context.status_text = 'Starting Load Flow'
-        results = lf.run_ac(self.context.network, parameters=self.context.lf_parameters, reporter=reporter)
-        self.context.status_text = 'Load Flow completed'
-        self.context.network_structure.refresh()
-        self.context.notify_selection_changed()  # hack to trigger refresh
-        print(results)
-        print(reporter)
+
+        def on_done():
+            self.context.network_structure.refresh()
+            self.context.notify_selection_changed()  # hack to trigger refresh
+            self.context.status_text = 'Load Flow completed'
+
+        def task():
+            if ac:
+                results = lf.run_ac(self.context.network, parameters=self.context.lf_parameters, reporter=reporter)
+            else:
+                results = lf.run_dc(self.context.network, parameters=self.context.lf_parameters, reporter=reporter)
+            print(results)
+            print(reporter)
+
+        self.context.start_long_running_task(name='Load Flow', target=task, on_done=on_done)
