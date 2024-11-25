dc364cf8548bef952513b91cdf21f66922d69da8
Damien Jeandemange <damien.jeandemange@gmail.com>
2024-11-25 07:18:50 +0100

    Fix logs view segfault on Linux
    Signed-off-by: Damien Jeandemange <damien.jeandemange@gmail.com>

--- yagat/frames/impl/logs_view.py
+++ yagat/frames/impl/logs_view.py
@@ -6,13 +6,20 @@
 # SPDX-License-Identifier: MPL-2.0
 #
 import logging
+import queue
 import tkinter as tk
 
 import tksheet as tks
 from pypowsybl import _pypowsybl
 
-# we will make this configurable some day
+# to be made configurable some day
 MAX_LOG_ROWS = 2000
+
+# how often we check for messages in the queue in milliseconds
+_LISTEN_LOOP_MS = 100
+
+# delay before redrawing sheet
+_AFTER_REDRAW_TIME_MS = 500
 
 
 class LogsView(tk.Frame, logging.Handler):
@@ -22,12 +29,13 @@ class LogsView(tk.Frame, logging.Handler):
         logging.Handler.__init__(self)
         logger = logging.getLogger()
         logger.addHandler(self)
-        self.sheet = tks.Sheet(self, index_align='left', after_redraw_time_ms=500)
+        self.sheet = tks.Sheet(self, index_align='left', after_redraw_time_ms=_AFTER_REDRAW_TIME_MS)
         self.sheet.hide(canvas="top_left")
         self.sheet.hide(canvas="row_index")
-        self.sheet.font(newfont=("Monaco", 12, "normal"))
+        self.sheet.font(
+            newfont=("Courier", 12, "normal"))  # not the best monospaced font but should work on all platforms
         self.sheet.set_header_data(c=0, value="Time")
-        self.sheet.set_column_widths([240, 90, 800])
+        self.sheet.set_column_widths([240, 90, 800])  # tksheet has typehint issue here but works
         self.sheet.set_header_data(c=1, value="Level")
         self.sheet.set_header_data(c=2, value="Message")
         self.sheet['A:C'].readonly(readonly=True)
@@ -46,15 +54,27 @@ class LogsView(tk.Frame, logging.Handler):
                                    'arrowkeys',
                                    )
         self.sheet.pack(fill="both", expand=True)
+        self.log_queue = queue.Queue()
+        self.after(_LISTEN_LOOP_MS, self.listen_queue)  # start listen loop
         logging.info(_pypowsybl.get_version_table())
 
     def emit(self, record):
-        msg = self.format(record)
-        self.sheet.insert_row(row=[record.asctime, record.levelname, msg], idx=0)
-        if record.levelname == 'WARNING':
-            self.sheet[0:1].highlight(bg='gold', redraw=False)
-        elif record.levelname == 'ERROR':
-            self.sheet[0:1].highlight(bg='salmon', redraw=False)
-        if len(self.sheet.data) > MAX_LOG_ROWS:
-            self.sheet.delete_row(idx=MAX_LOG_ROWS, redraw=False)
-        self.sheet.set_cell_size_to_text(row=0, column=2, only_set_if_too_small=True)
+        self.log_queue.put(record)
+
+    def listen_queue(self):
+        while self.log_queue.qsize():
+            try:
+                record = self.log_queue.get()
+                msg = self.format(record)
+                self.sheet.insert_row(row=[record.asctime, record.levelname, msg], idx=0)
+                if record.levelname == 'WARNING':
+                    self.sheet[0:1].highlight(bg='gold', redraw=False)
+                elif record.levelname == 'ERROR':
+                    self.sheet[0:1].highlight(bg='salmon', redraw=False)
+                if len(self.sheet.data) > MAX_LOG_ROWS:
+                    self.sheet.delete_row(idx=MAX_LOG_ROWS, redraw=False)
+                self.sheet.set_cell_size_to_text(row=0, column=2, only_set_if_too_small=True)
+            except queue.Empty:
+                pass
+        # re-listen
+        self.after(_LISTEN_LOOP_MS, self.listen_queue)
